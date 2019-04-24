da2c2a58953fbb6747bff79107e3f7ae29f8e56f
Omer Turhan <omer.turhan@accenture.com>
2019-04-24 18:33:44 +0300

    ball grid pulse animation begin time set to 0 in order to eliminate animation stuck when using with hero

--- Source/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallGridPulse.swift
+++ Source/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallGridPulse.swift
@@ -28,42 +28,42 @@
 import UIKit
 
 class NVActivityIndicatorAnimationBallGridPulse: NVActivityIndicatorAnimationDelegate {
-
+    
     func setUpAnimation(in layer: CALayer, size: CGSize, color: UIColor) {
         let circleSpacing: CGFloat = 2
         let circleSize = (size.width - circleSpacing * 2) / 3
         let x = (layer.bounds.size.width - size.width) / 2
         let y = (layer.bounds.size.height - size.height) / 2
         let durations: [CFTimeInterval] = [0.72, 1.02, 1.28, 1.42, 1.45, 1.18, 0.87, 1.45, 1.06]
-        let beginTime = CACurrentMediaTime()
+        let beginTime:CFTimeInterval = 0
         let beginTimes: [CFTimeInterval] = [ -0.06, 0.25, -0.17, 0.48, 0.31, 0.03, 0.46, 0.78, 0.45]
         #if swift(>=4.2)
         let timingFunction = CAMediaTimingFunction(name: .default)
         #else
         let timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionDefault)
         #endif
-
+        
         // Scale animation
         let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-
+        
         scaleAnimation.keyTimes = [0, 0.5, 1]
         scaleAnimation.timingFunctions = [timingFunction, timingFunction]
         scaleAnimation.values = [1, 0.5, 1]
-
+        
         // Opacity animation
         let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
-
+        
         opacityAnimation.keyTimes = [0, 0.5, 1]
         opacityAnimation.timingFunctions = [timingFunction, timingFunction]
         opacityAnimation.values = [1, 0.7, 1]
-
+        
         // Animation
         let animation = CAAnimationGroup()
-
+        
         animation.animations = [scaleAnimation, opacityAnimation]
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-
+        
         // Draw circles
         for i in 0 ..< 3 {
             for j in 0 ..< 3 {
@@ -72,7 +72,7 @@ class NVActivityIndicatorAnimationBallGridPulse: NVActivityIndicatorAnimationDel
                                    y: y + circleSize * CGFloat(i) + circleSpacing * CGFloat(i),
                                    width: circleSize,
                                    height: circleSize)
-
+                
                 animation.duration = durations[3 * i + j]
                 animation.beginTime = beginTime + beginTimes[3 * i + j]
                 circle.frame = frame
